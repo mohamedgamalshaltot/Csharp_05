2c72ade3b4ccb60c0b580a3a6ba9afe46d10e51b
mohamedgamalshaltot <mohmedgamalshaltot@gmail.com>
2026-09-09 17:14:53 +0300

    Add project files.

---
+++ c#_Bascis05/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace c__Bascis05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
