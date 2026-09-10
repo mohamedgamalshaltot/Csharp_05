674887f51e1f4fff55c7cb39132f0e6bcf7d71db
mohamedgamalshaltot <mohmedgamalshaltot@gmail.com>
2026-09-10 12:06:06 +0300

    Question 02

--- c#_Bascis05/Program.cs
+++ c#_Bascis05/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace c__Bascis05
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace c__Bascis05
 {
     internal class Program
     {
@@ -9,6 +11,16 @@
             //the second price(index 1).
             double[] prices = { 25.5, 40.0, 33.75 };
             Console.WriteLine(prices[1]);
+
+            #endregion
+            #region Question 02
+            //2-Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and
+            //shelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.
+            int[,] shelfCopies = { 
+                { 3, 5 }, { 1, 4 } 
+            };
+            Console.WriteLine(shelfCopies[1,0]);
+
 
             #endregion
         }
